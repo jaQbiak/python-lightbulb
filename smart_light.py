5409c000b84b65e95b630e51d200ee9541917577
Patryk Jakubiak <jaqbiak88@gmail.com>
2022-02-10 20:59:44 +0100

    fix

--- smart_light.py
+++ smart_light.py
@@ -10,6 +10,7 @@ import os
 
 load_dotenv()
 
+
 def connect_to_device():
     device_id = os.getenv('DEVICE_ID')
     ip_address = os.getenv('IP_ADDRESS')
@@ -18,17 +19,17 @@ def connect_to_device():
     device.set_version(3.3)
     return device
 
+
 def get_color():
     device = connect_to_device()
     device.status()
 
-root = tkinter.Tk()
-root.geometry('200x200')
 
 def change_color(red, green, blue):
     device = connect_to_device()
     device.set_colour(red, green, blue)
-    
+
+
 def choose_color():
     color = colorchooser.askcolor()
     red = math.floor(color[0][0])
@@ -36,28 +37,35 @@ def choose_color():
     blue = math.floor(color[0][2])
     change_color(red, green, blue)
 
+
 def white():
     device = connect_to_device()
     device.set_white(255, 255)
+
 
 def change_brightness():
     device = connect_to_device()
     device.set_brightness(brightness_slider.get())
 
+
 def turn_on():
     device = connect_to_device()
     device.turn_on()   
+
 
 def turn_off():
     device = connect_to_device()
     device.turn_off()   
 
-color_button = Button(root, text="Choose color", command=choose_color).pack()
-white_button = Button(root, text="White", command=white).pack()
+
+root = tkinter.Tk()
+root.geometry('200x200')
+Button(root, text="Choose color", command=choose_color).pack()
+Button(root, text="White", command=white).pack()
 brightness_slider = Scale(root, from_=25, to=255, orient=HORIZONTAL)
 brightness_slider.pack()
-brightness_button = Button(root, text="Change brightness", command=change_brightness).pack()
-turn_on_button = Button(root, text="On", command=turn_on).pack()
-turn_off_button = Button(root, text="Off", command=turn_off).pack()
+Button(root, text="Change brightness", command=change_brightness).pack()
+Button(root, text="On", command=turn_on).pack()
+Button(root, text="Off", command=turn_off).pack()
 root.mainloop()
     
